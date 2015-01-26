c664bd877a17d7100662fdf35db5284812330dca
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-26 17:13:27 -0500

    Move Server Loop and Properties To Class Server
    Move Server Loop and Properties to a class called Server because this is
    allow the code to be portable to other project if needed.

--- server.rb
+++ server.rb
@@ -40,26 +40,35 @@ def get_response_file(first_line_from_client_header)
 	first_line_from_client_header.gsub(/GET \//, "").gsub(/\ HTTP.*/, "")
 end
 
-host = 'localhost'
-port = 2000
+class Server
+	def initialize(host, port)
+		@host = host
+		@port = port
+	end
+	
+	def start_server
+		server = TCPServer.open(@host, @port)                 # Socket to listen to defined host and port
+		puts "Server started on #{@host}:#{@port} ..."        # Output to stdout that server started
 
-server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
-puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
+		loop do                                             # Server runs forever
+		  client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
 
-loop do                                             # Server runs forever
-  client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
+		  lines = []
 
-  lines = []
+		  while (line = client.gets.chomp) && !line.empty?  # Read the request and collect it until it's empty
+		    lines << line
+		  end
 
-  while (line = client.gets.chomp) && !line.empty?  # Read the request and collect it until it's empty
-    lines << line
-  end
+		  puts lines                                        # Output the full request to stdout
 
-  puts lines                                        # Output the full request to stdout
-
-  filename = get_response_file(lines[0])
-  header_content = get_content_from(filename)
-  
-  client.puts(header_content) # Output the current time to the client
-  client.close                                      		# Disconnect from the client
+		  filename = get_response_file(lines[0])
+		  header_content = get_content_from(filename)
+		  
+		  client.puts(header_content) # Output the current time to the client
+		  client.close                                      		# Disconnect from the client
+		end
+	end
 end
+
+localhost = Server.new("localhost", 2000)
+localhost.start_server
